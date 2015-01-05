8b96073a388a45dc65d2d50f155dd0dc14cb53da
Aluriak <lucas.bourneuf@laposte.net>
2015-01-06 00:00:54 +0100

    more complete and interesting using of Pokemon class methods

--- pokemonViewer.py
+++ pokemonViewer.py
@@ -8,6 +8,7 @@
 # IMPORTS               #
 #########################
 import pickle
+import random
 from common.pokemon import Pokemon
 from common.common import FILE_POKEMON
 
@@ -39,7 +40,20 @@ from common.common import FILE_POKEMON
 if __name__ == '__main__':
     with open(FILE_POKEMON, "rb") as fpok:
         pokemons = pickle.load(fpok)
+
+
     for pokemon in pokemons:
-        print(pokemon)
+        # quick printing of a Pokemon instance
+        #print(pokemon) 
+        # that is equivalent to:
+        print(pokemon.name + ' (' + ' and '.join(pokemon.types) + '): ' + ''.join(pokemon.dna))
+        # a Pokémon object know its distance to another one
+        random_pokemon = random.choice(pokemons) 
+        print('Hamming distance between', pokemon.name, 'and', random_pokemon.name, 'is', 
+              pokemon.distTo(random_pokemon) # by default its hamming distance
+             )
+
+
+
 
 
