644fffcea3079edc22d5404ab530cbf2fd4db05c
Aluriak <lucas.bourneuf@laposte.net>
2015-01-06 00:02:48 +0100

    pokemons.dat file go in data repertory

--- common/common.py
+++ common/common.py
@@ -16,7 +16,7 @@ import sys
 #########################
 # PRE-DECLARATIONS      #
 #########################
-FILE_POKEMON = "pokemons.dat"
+FILE_POKEMON = "data/pokemons.dat"
 URL_POKEBIP_POKEDEX = "http://www.pokebip.com/pokemon/pokedex/"
 URL_POKEBIP_SEARCH_POKEDEX = 'http://www.pokebip.com/pokedex/index.php?phppage=gen5/liste&poke_tri=1'
 
